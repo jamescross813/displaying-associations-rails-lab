e395618e15bd3f014d4c790d76d6d55b79550026
jamescross <crossjames813@gmail.com>
2021-06-12 21:16:02 -0500

    commit tree 2c2610a2df910fb62abccba1022d6012c4ca6669

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    song = self.artist_id
+    artist = Artist.find_by(id: song).name
+  end
+
+  def artist_and_title
+    artist_name + " - " + self.title
+  end
 end
